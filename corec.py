125f72d0a3d93292628c65f0672be7f4f9ba54eb
Jonas Cowley <jcowley@purdue.edu>
2025-09-04 13:17:34 -0400

    Basic Co-Rec and Menu API Calls

---
+++ corec.py
@@ -0,0 +1,18 @@
+import requests
+
+building_ids = ",".join(map(str, [7, 11, 12]))
+start_date = "8/21/2025"
+end_date = "10/9/2025"
+
+url= (
+    f"https://itap.purdue.edu/apps/emsproxy/api/hours?"
+    f"buildingIds={building_ids}&startDate={start_date}&endDate={end_date}"
+)
+
+headers = {"Accept": "application/json"}
+
+response = requests.get(url, headers=headers)
+
+data = response.json()
+
+print(data)
--- menu.py
+++ menu.py
@@ -1,1 +1,51 @@
+import requests
 
+url = "https://api.hfs.purdue.edu/menus/v3/GraphQL"
+
+query = """
+query getLocationMenu($name: String!, $date: Date!) {
+    diningCourtByName(name: $name) {
+        name
+        formalName
+        dailyMenu(date : $date) {
+            meals {
+                name
+                startTime
+                endTime
+                stations {
+                    name
+                    items {
+                        item {
+                            name
+                            traits {
+                                name
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+"""
+
+variables = {"name": "Earhart", "date": "2025-09-04"}
+
+response = requests.post(url, json={
+    "operationName": "getLocationMenu",
+    "variables": variables,
+    "query": query
+})
+
+data = response.json()
+meals = data["data"]["diningCourtByName"]["dailyMenu"]["meals"]
+
+for meal in meals:
+    print(f"\n=== {meal['name']} ({meal['startTime']} - {meal['endTime']}) ===")
+    for station in meal['stations']:
+        print(f" Station: {station['name']}")
+        for item in station['items']:
+            item_name = item['item']['name']
+            print(f"   - {item_name}")
+
+# print(data)
